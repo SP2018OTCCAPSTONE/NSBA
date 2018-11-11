bad9d3ee9ba10e1db51b70533d641fcd8d641473
Kelli <kelli_stasiak@yahoo.com>
2018-11-11 11:49:10 -0600

    Putting the footer back to original

--- includes/footer.php
+++ includes/footer.php
@@ -1,33 +1,5 @@
 <!-- ***Footer*********************************-->
         </div>
-        <div id = "contact-info">
-            <strong>North Springfield Betterment Association</strong>
-            " | "
-            <i class = "icon-home">
-                ::before
-            </i>
-            " 900 N. Benton Ave  Springfield, MO | "
-            <i class ="icon-envelope">
-                ::before
-            </i>
-            <a href="mailto:info@nsbanet.com">info@nsbanet.com</a> 
-        </div>
-        <div id = "footer">
-            <div id = "sitemap">
-                <ul>
-                    <li><a href ="index.html">Home</a></li>
-                    <li><a href = "community-projects.html">Events/News</a></li>
-                    <li><a href = "community-membership-members.html">Members</a></li>
-                    <li><a href = "community-newsletter.html">Newsletter</a></li>
-                    <li><a href = "sitemap.html">Sitemap</a></li>
-                </ul>
-                <div class ="clear"></div>
-            </div>
-            <div id="copyright">
-                <a href="http://www.commpub.com/" target ="_blank" rel="nofollow">...</a>
-            </div>
-            <div class = "clear"></div>
-        </div>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
